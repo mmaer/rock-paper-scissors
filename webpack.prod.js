075349f0a97f8ce9d22696641245cf666722914c
Kamil Kruczek <kamilkruczek0@hotmail.com>
2020-04-24 19:55:19 +0200

    Added webpack-bundle-analyzer.

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,17 @@
 const merge = require('webpack-merge');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const NullPlugin = require('webpack-null-plugin');
+
 const common = require('./webpack.common.js');
+
+const ANALYZER = process.env.ANALYZER;
 
 module.exports = merge(common, {
   mode: 'production',
   entry: [
     './src/index.js'
   ],
+  plugins: [
+    ANALYZER === 'true' ? new BundleAnalyzerPlugin() : new NullPlugin(),
+  ]
 });
